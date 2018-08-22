52eb527f1a38ea60e5a531855fd13a0a218946b1
napatsakorn <napatsakorn.pis@mahidol.ac.th>
2018-08-22 09:49:42 +0700

    Add data of funding_source sheet to database

--- load_data.py
+++ load_data.py
@@ -8,17 +8,27 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 
-staff_df = read_excel('sample_fundingdata.xlsx', sheet_name='Staff')
+#staff_df = read_excel('sample_fundingdata.xlsx', sheet_name='Staff')
+fund_df = read_excel('sample_fundingdata.xlsx', sheet_name='FundingSource')
 
-for ix,row in staff_df.iterrows():
-    st = Staff(
-        staff_firstname=row['staff_firstname'],
-        staff_lastname=row['staff_lastname'],
-        staff_email=row['staff_email'],
-        department_name=row['department_name']
+for ix,row in fund_df.iterrows():
+    fund = FundingSource(
+        funding_source = row['funding_source'],
+        funding_agency = row['funding_agency'],
+        duration = row['duration'],
+        funding_contract = row['funding_contract']
     )
-    session.add(st)
+    session.add(fund)
 session.commit()
+#for ix,row in staff_df.iterrows():
+#    st = Staff(
+#        staff_firstname=row['staff_firstname'],
+#        staff_lastname=row['staff_lastname'],
+#        staff_email=row['staff_email'],
+#        department_name=row['department_name']
+#    )
+#    session.add(st)
+#session.commit()
 
 #st = session.query(Staff).filter(Staff.staff_email=='napat.son').first()
 #st = session.query(Staff).filter(Staff.staff_email==row['staff_email']).first()
